13eca013cb4dbfa6b18886546ab41e0ed2aeef5b
nataliepw2003 <wrightn2017@hardingacademy.me>
2017-06-22 11:34:54 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -46,3 +46,87 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+def hangman():
+    winfactor=0
+    listc=[]
+    wronglist=[]
+    alphalist= ["a","b","c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    print "Welcome to hangman"
+    #word=choose_word(wordlist)
+    word = list(choose_word(wordlist))
+    print word
+    num_guess=len(word)
+    length=len(word)
+    LIST = [len(word) * "_ "]  #####
+    print "The word is", length,"letters long. You have", length, "guesses."
+    # for x in range(0,int(length)):
+    #             listc.append("_ ")
+    while num_guess!=0:
+                print ""
+                guess = raw_input("Guess a letter: ")
+                print ""
+                if guess in alphalist:
+                    if guess in word:
+                        print guess,"is in this word."
+                        listc.append(guess)
+
+                       #WHERE THE LETTER IS LOCATED, CODE  (INC)
+                        # if guess==word[0]:
+                        #     print ""
+                        #     print guess,"is the first letter."
+                        #     print ""
+                        # if guess==word[int(length)-1]:
+                        #     print ""
+                        #     print guess,"is the last letter."
+                        #     print ""
+
+                    elif guess in listc:
+                        print "You already guessed this. Please guess again."
+                    elif guess in wronglist:
+                        print "You already guessed this. Please guess again"
+                    else:
+                        print guess, "was not correct."
+                        wronglist.append(guess)
+                        num_guess=num_guess-1
+                else:
+                    print "Your guess was either capitalized, more than one letter, or it is not in the alphabet. Guess again."
+                    print ""
+                if num_guess==0:
+                    print ""
+                    print "YOU RAN OUT OF GUESSES"
+                    print" "
+                    print "The correct word was", word
+                    print ""
+                    print"--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+                    break
+                print "wrong letters=", wronglist
+                print""
+                print "correct letters=", listc
+                print ""
+                print "You have", num_guess, "guesses left."
+                if listc==word:
+                    print"  Y"
+                    print"      O"
+                    print"          U"
+                    print"  W"
+                    print"      O"
+                    print"          N"
+                    print"--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+                    break
+
+
+hangman()
+#
+# print word  #
+# if guess in word:  #
+#     if guess == word[0]:  #
+#         LIST = [guess] + ((length - 1) * "_ ")  #
+#         print LIST  #
+#     else:  #
+#         print LIST  #
+# playagain= raw_input("Want to play again?(y or n)")
+#
+# if playagain==y:
+#     hangman()
+# else:
+#     break
